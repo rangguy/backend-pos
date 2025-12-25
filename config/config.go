07f85e04b69101962c9717a1288aa7728efbb328
rangguy <ranggadwi100@gmail.com>
2025-12-25 18:14:42 +0700

    update config lagi

--- config/config.go
+++ config/config.go
@@ -11,43 +11,67 @@ import (
 var Config AppConfig
 
 type AppConfig struct {
-	Port                  int      `json:"port"`
-	AppName               string   `json:"appName"`
-	AppEnv                string   `json:"appEnv"`
-	SignatureKey          string   `json:"signatureKey"`
-	Database              Database `json:"database"`
-	RateLimiterMaxRequest int      `json:"rateLimiterMaxRequest"`
-	RateLimiterTimeSecond int      `json:"rateLimiterTimeSecond"`
-	JwtSecretKey          string   `json:"jwtSecretKey"`
-	JwtExpirationTime     int      `json:"jwtExpirationTime"`
+	Port                  int
+	AppName               string
+	AppEnv                string
+	SignatureKey          string
+	Database              Database
+	RateLimiterMaxRequest int
+	RateLimiterTimeSecond int
+	JwtSecretKey          string
+	JwtExpirationTime     int
 }
 
 type Database struct {
-	Host                  string `json:"host"`
-	Port                  int    `json:"port"`
-	Name                  string `json:"name"`
-	Username              string `json:"username"`
-	Password              string `json:"password"`
-	MaxOpenConnection     int    `json:"maxOpenConnection"`
-	MaxLifetimeConnection int    `json:"maxLifetimeConnection"`
-	MaxIdleConnection     int    `json:"maxIdleConnection"`
-	MaxIdleTime           int    `json:"maxIdleTime"`
+	Host                  string
+	Port                  int
+	Name                  string
+	Username              string
+	Password              string
+	MaxOpenConnection     int
+	MaxLifetimeConnection int
+	MaxIdleConnection     int
+	MaxIdleTime           int
 }
 
 func Init() {
-	err := util.BindFromJSON(&Config, "config.json", ".")
-	if err != nil {
-		logrus.Infof("failed to bind config: %v", err)
-		err = util.BindFromConsul(&Config, os.Getenv("CONSUL_HTTP_URL"), os.Getenv("CONSUL_HTTP_KEY"))
-		if err != nil {
-			panic(err)
+	// 1️⃣ Default dari ENV (SOURCE OF TRUTH)
+	loadFromEnv()
+
+	// 2️⃣ Optional: override dari config.json (LOCAL DEV)
+	if err := util.BindFromJSON(&Config, "config.json", "."); err == nil {
+		logrus.Info("Config loaded from config.json")
+	}
+
+	// 3️⃣ Optional: override dari Consul
+	if os.Getenv("CONSUL_HTTP_URL") != "" {
+		if err := util.BindFromConsul(
+			&Config,
+			os.Getenv("CONSUL_HTTP_URL"),
+			os.Getenv("CONSUL_HTTP_KEY"),
+		); err != nil {
+			logrus.Warnf("Failed to load config from Consul: %v", err)
 		}
 	}
 
+	// 4️⃣ ENV selalu override terakhir
 	overrideFromEnv()
 
-	logrus.Infof("Config loaded: %s (env: %s, port: %d)", Config.AppName, Config.AppEnv, Config.Port)
-	logrus.Infof("Database: %s@%s:%d/%s", Config.Database.Username, Config.Database.Host, Config.Database.Port, Config.Database.Name)
+	validate()
+
+	logrus.Infof(
+		"Config loaded: %s (%s) port=%d",
+		Config.AppName,
+		Config.AppEnv,
+		Config.Port,
+	)
+	logrus.Infof(
+		"Database: %s@%s:%d/%s",
+		Config.Database.Username,
+		Config.Database.Host,
+		Config.Database.Port,
+		Config.Database.Name,
+	)
 }
 
 func loadFromEnv() {
@@ -72,76 +96,62 @@ func loadFromEnv() {
 		JwtSecretKey:          getEnv("JWT_SECRET_KEY", ""),
 		JwtExpirationTime:     getEnvInt("JWT_EXPIRATION_TIME", 1440),
 	}
-
-	if Config.Database.Password == "" {
-		logrus.Warn("DB_PASSWORD is not set!")
-	}
-	if Config.JwtSecretKey == "" {
-		logrus.Warn("JWT_SECRET_KEY is not set!")
-	}
 }
 
 func overrideFromEnv() {
-	if port := os.Getenv("APP_PORT"); port != "" {
-		if p, err := strconv.Atoi(port); err == nil {
-			Config.Port = p
-		}
+	if v := os.Getenv("APP_PORT"); v != "" {
+		Config.Port, _ = strconv.Atoi(v)
 	}
-
-	if appName := os.Getenv("APP_NAME"); appName != "" {
-		Config.AppName = appName
+	if v := os.Getenv("APP_NAME"); v != "" {
+		Config.AppName = v
 	}
-
-	if appEnv := os.Getenv("APP_ENV"); appEnv != "" {
-		Config.AppEnv = appEnv
+	if v := os.Getenv("APP_ENV"); v != "" {
+		Config.AppEnv = v
 	}
-
-	if signKey := os.Getenv("SIGNATURE_KEY"); signKey != "" {
-		Config.SignatureKey = signKey
+	if v := os.Getenv("SIGNATURE_KEY"); v != "" {
+		Config.SignatureKey = v
 	}
-
-	if dbHost := os.Getenv("DB_HOST"); dbHost != "" {
-		Config.Database.Host = dbHost
+	if v := os.Getenv("DB_HOST"); v != "" {
+		Config.Database.Host = v
 	}
-
-	if dbPort := os.Getenv("DB_PORT"); dbPort != "" {
-		if p, err := strconv.Atoi(dbPort); err == nil {
-			Config.Database.Port = p
-		}
+	if v := os.Getenv("DB_PORT"); v != "" {
+		Config.Database.Port, _ = strconv.Atoi(v)
 	}
-
-	if dbName := os.Getenv("DB_NAME"); dbName != "" {
-		Config.Database.Name = dbName
+	if v := os.Getenv("DB_NAME"); v != "" {
+		Config.Database.Name = v
 	}
-
-	if dbUser := os.Getenv("DB_USER"); dbUser != "" {
-		Config.Database.Username = dbUser
+	if v := os.Getenv("DB_USER"); v != "" {
+		Config.Database.Username = v
 	}
-
-	if dbPass := os.Getenv("DB_PASSWORD"); dbPass != "" {
-		Config.Database.Password = dbPass
+	if v := os.Getenv("DB_PASSWORD"); v != "" {
+		Config.Database.Password = v
 	}
-
-	if jwtKey := os.Getenv("JWT_SECRET_KEY"); jwtKey != "" {
-		Config.JwtSecretKey = jwtKey
+	if v := os.Getenv("JWT_SECRET_KEY"); v != "" {
+		Config.JwtSecretKey = v
 	}
-
-	if jwtExp := os.Getenv("JWT_EXPIRATION_TIME"); jwtExp != "" {
-		if exp, err := strconv.Atoi(jwtExp); err == nil {
-			Config.JwtExpirationTime = exp
-		}
+	if v := os.Getenv("JWT_EXPIRATION_TIME"); v != "" {
+		Config.JwtExpirationTime, _ = strconv.Atoi(v)
 	}
-
-	if maxReq := os.Getenv("RATE_LIMITER_MAX_REQUEST"); maxReq != "" {
-		if req, err := strconv.Atoi(maxReq); err == nil {
-			Config.RateLimiterMaxRequest = req
-		}
+	if v := os.Getenv("RATE_LIMITER_MAX_REQUEST"); v != "" {
+		Config.RateLimiterMaxRequest, _ = strconv.Atoi(v)
 	}
+	if v := os.Getenv("RATE_LIMITER_TIME_SECOND"); v != "" {
+		Config.RateLimiterTimeSecond, _ = strconv.Atoi(v)
+	}
+}
 
-	if timeSec := os.Getenv("RATE_LIMITER_TIME_SECOND"); timeSec != "" {
-		if sec, err := strconv.Atoi(timeSec); err == nil {
-			Config.RateLimiterTimeSecond = sec
-		}
+func validate() {
+	if Config.Database.Host == "" {
+		logrus.Fatal("DB_HOST is required")
+	}
+	if Config.Database.Name == "" {
+		logrus.Fatal("DB_NAME is required")
+	}
+	if Config.Database.Username == "" {
+		logrus.Fatal("DB_USER is required")
+	}
+	if Config.JwtSecretKey == "" {
+		logrus.Fatal("JWT_SECRET_KEY is required")
 	}
 }
 
